f20b4ad1b013641484b3424e04c60330e297ad3f
JonathanMoroy <jonathan.moroy@gmail.com>
2017-02-01 15:12:52 +0100

    Début du projet

---
+++ biography.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link type="text/css" rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php  ?>
+
+  </body>
+</html>
